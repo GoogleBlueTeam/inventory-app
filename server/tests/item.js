f646e7054a851cafda2d6bda7376aa6ddde1c763
Jared Chapman <thejaredchapman@gmail.com>
2022-11-08 11:46:59 -0600

    wrote test for item

---
+++ server/tests/item.js
@@ -0,0 +1,10 @@
+class Item {
+    constructor (title, price, description, category, image) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.image = image;
+}}
+
+module.exports = Item
